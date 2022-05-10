53e1eeac27fe7ca7dccb8b42f975293ad2822108
voltagedomo <98976613+voltagedomo@users.noreply.github.com>
2022-05-10 10:26:10 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ cnv.width = innerWidth - 2;
 
 // global variables
 let mouseX, mouseY;
-let circleX = 200;
+let circleX = 1150; // previously 200
 let circleY = 105;
 let ballSpeed = 0;
 let verticalBallSpeed = 0;
@@ -54,59 +54,84 @@ function pong() {
     if (circleX >= innerWidth - 10) {
         circleX = innerWidth - 7;
         ballSpeed = 0;
-    } else if (circleX <= 10) {
+        verticalBallSpeed = 0;
+    } else if (circleX <= 7) {
         circleX = 5;
         ballSpeed = 0;
+        verticalBallSpeed = 0;
     }
 
     // if ball hits vertical screen limits, reverse verticalBallSpeed
     if (circleY >= innerHeight - 11) {
-        
+        verticalBallSpeed = verticalBallSpeed * -1;
     } else if (circleY < 11) {
-        circleY = 11;
-    } 
+        verticalBallSpeed = verticalBallSpeed * -1;
+    }
 
     // move player by x speed and y speed
     player.y += player.speed;
     player2.y += player2.speed;
 
+    // ORIGINAL CODE
     // if pong ball touches player, change ball speed
-    if (player.x + 7 >= circleX - 2 && player.x - 7 < circleX + 2 && player.y <= circleY + 2 && circleY + 2 < player.y + 100) {
-        verticalBallSpeed = 0;
+    // if (player.x + 7 >= circleX - 2 && player.x - 7 < circleX + 2 && player.y <= circleY + 2 && circleY + 2 < player.y + 100) {
+
+    // move pong ball after it hits player
+    if (player.x + 8 > circleX - 7 && player.x - 7 < circleX + 2 && player.y < circleY + 2 && circleY + 2 < player.y + 100) {
         ballSpeed += 6;
-        // get random number between -10 and 10
-        num = Math.floor(Math.random() * 10) + 1; // get number between 1 and 10
+        circleX = player.x + 15;
+        num = 0;
+        num = Math.floor(Math.random() * 10) + 1; // random number 1-10
         num *= Math.round(Math.random()) ? 1 : -1; // add minus sign 50% of times
         console.log(num);
-        // replace verticalBallSpeed with random number
         verticalBallSpeed += num;
     }
 
+    // ORIGINAL CODE
     // if pong ball touches player2, change ball speed
+    // if (player2.x - 7 <= circleX + 2 && player2.x + 7 > circleX - 2 && player2.y <= circleY + 2 && circleY + 2 < player2.y + 100) {
+
+    // move pong ball in vertical direction based on where it hits player2
+    // upwards
     if (player2.x - 7 <= circleX + 2 && player2.x + 7 > circleX - 2 && player2.y <= circleY + 2 && circleY + 2 < player2.y + 100) {
-        verticalBallSpeed = 0;
         ballSpeed -= 6;
-        // get random number between -10 and 10
-        num = Math.floor(Math.random() * 10) + 1; // get number between 1 and 10
+        circleX = player2.x - 5;
+        num = 0;
+        num = Math.floor(Math.random() * 10) + 1; // random number 1-10
         num *= Math.round(Math.random()) ? 1 : -1; // add minus sign 50% of times
         console.log(num);
-        // replace verticalBallSpeed with random number
         verticalBallSpeed += num;
     }
 
-    circleY += verticalBallSpeed;
 
     // limit for ballSpeed
-    if (ballSpeed > 4) {
-        ballSpeed = 4;
-    } else if (ballSpeed < -4) {
-        ballSpeed = -4;
+    if (ballSpeed <= 6 && ballSpeed >= 0.1) {
+        ballSpeed = 6;
+    } else if (ballSpeed >= -6 && ballSpeed <= -0.1) {
+        ballSpeed = -6;
+    }
+
+    // limit for verticalBallSpeed
+    if (verticalBallSpeed > 8) {
+        verticalBallSpeed = 8;
+    } else if (verticalBallSpeed < -8) {
+        verticalBallSpeed = -8;
+    }
+
+
+    if (ballSpeed == 0 && circleX > 5 && circleX < innerWidth - 9) {
+        num = 0;
+        num = Math.floor(Math.random() * 10) + 1; // random number 1-10
+        num *= Math.round(Math.random()) ? 1 : -1; // add minus sign 50% of times
+        console.log(num);
+        ballSpeed = num;
     }
 
     // actualy move the ball
-    circleX += ballSpeed
+    circleX += ballSpeed;
+    circleY += verticalBallSpeed;
 
-    // DRAW
+    // DRAW ---------------------------------------------------------------------------------------------- 
     // draw background to erase history
     ctx.fillStyle = `black`;
     ctx.fillRect(0, 0, innerWidth, innerHeight);
@@ -130,8 +155,9 @@ function keydownHandler(event) {
     console.log(event.code);
     if (event.code == `Space`) { // place ball and give ball speed when space pressed
         circleX = 200;
-        circleY = 105;
-        ballSpeed = -4;
+        circleY = player.y + 50;
+        ballSpeed = -6;
+        verticalBallSpeed = 0;
     } else if (event.code == `KeyD`) { // stop player movement when E is pressed
         player.speed = 0;
     } else if (event.code == `Numpad4`) { // stop player2 movement when numpad4 is pressed
@@ -150,4 +176,3 @@ function keydownHandler(event) {
         }
     }
 }
-
